6d5588b7d64a6a92a5e9aae109a572d4006a6975
bagasmuharom <bagashidayat45@gmail.com>
2017-07-29 19:11:40 +0700

    menambah route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/',function(){
+    return view('welcome');
+})->middleware('guest');
 
 Route::get('send',function(){
     Mail::to('rafy683@gmail.com')->send(new \PMW\Mail\RegisterMail());
@@ -28,15 +21,34 @@ Route::get('tes',function(){
     })->export('xls');
 });
 
-Route::get('/home', function () {
+Route::get('/dashboard', function () {
     return view('home');
-})->name('home');
+})->name('dashboard')->middleware('auth');
 
 Route::group(['middleware' => 'auth'] ,function(){
+
+    Route::group(['middleware' => 'ketuatim'],function(){
+        
+    });
+    
+    Route::group(['middleware' => 'reviewer'],function(){
+
+    });
+
+    Route::group(['middleware' => 'adminfakultas'],function(){
+
+    });
+
+    Route::group(['middleware' => 'adminuniv'],function(){
+
+    });
+
     Route::get('gantipassword',function(){
         return view('gantipass');
     });
+
     Route::post('gantipassword','SettingsController@gantiPassword')->name('gantipassword');
+
 });
 
 /*
