a1044e14ed1f0ab6ad57a54b8ec6189b56e9cb84
bagasmuharom <bagashidayat45@gmail.com>
2017-08-03 15:20:06 +0700

    Update route web.php

--- routes/web/web.php
+++ routes/web/web.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /**
@@ -19,24 +18,47 @@ Route::group(['middleware' => 'auth'] ,function(){
          * dilakukan pengecekan apakah user sedang login atau belum.
          * Jika belum, maka akan diarahkan ke halaman login.
          */
-        Route::get('dashboard',function(){
-            return view('dashboard');
-        })->name('dashboard');
+        Route::get('dashboard', [
+            'uses' => 'DashboardController@index',
+            'as' => 'dashboard'
+        ]);
 
         Route::group(['prefix' => 'cari'],function(){
+
             Route::post('carimahasiswa',[
                 'uses' => 'PencarianController@cariMahasiswa',
                 'as' => 'cari.mahasiswa'
             ]);
+
+        });
+
+        Route::group(['prefix' => 'undang'],function(){
+
+            Route::get('anggota', function(){
+                return view('mahasiswa.undanganggota');
+            })->middleware('mahasiswa');
+
+            Route::post('anggota', [
+                'uses' => 'UndanganTimController@buatUndangan',
+                'as' => 'undang.anggota',
+                'middleware' => 'mahasiswa'
+            ]);
+
+            Route::post('terima',[
+                'uses' => 'UndanganTimController@terimaUndangan',
+                'as' => 'terima.undangan.tim',
+                'middleware' => 'mahasiswa'
+            ]);
+
         });
 
     });
 
-    Route::get('pengaturan',function(){
+    Route::get('pengaturan', function(){
          return view('pengaturan');
     })->name('pengaturan');
 
-    Route::group(['prefix' => 'ubah'],function(){
+    Route::group(['prefix' => 'ubah'], function(){
 
         Route::get('password',function(){
             return view('ubahpassword');
@@ -57,67 +79,3 @@ Route::group(['middleware' => 'auth'] ,function(){
 |-----
 */
 Auth::routes();
-=======
-<?php
-
-/**
- * Ketika membuka halaman awal,
- * maka user akan diarahkan ke halaman dashboard
- */
-
-Route::get('/',function(){
-    return redirect()->route('dashboard');
-});
-
-Route::group(['middleware' => 'auth'] ,function(){
-
-    Route::group(['middleware' => 'profil'], function(){
-
-        /**
-         * Jika user mmebuka halaman dashboard, maka akan
-         * dilakukan pengecekan apakah user sedang login atau belum.
-         * Jika belum, maka akan diarahkan ke halaman login.
-         */
-        Route::get('dashboard',function(){
-            return view('dashboard');
-        })->name('dashboard');
-
-        Route::group(['prefix' => 'cari'],function(){
-//            Route::post('carimahasiswa',[
-//                'uses' => '',
-//                'as' => 'cari.mahasiswa'
-//            ]);
-        });
-
-    });
-
-    Route::get('pengaturan',function(){
-         return view('pengaturan');
-    })->name('pengaturan');
-
-    Route::group(['prefix' => 'ubah'],function(){
-
-        Route::get('password',function(){
-            return view('ubahpassword');
-        })->name('ubah.password');
-
-        Route::post('profil',[
-            'uses' => 'UserController@editProfil',
-            'as' => 'ubah.profil'
-        ]);
-        
-    });
-
-    Route::get('role',function(){
-        return Illuminate\Support\Facades\Auth::user()->hakAkses()->where('nama','Ketua Tim')->count();
-    });
-
-});
-
-/*
-|-----
-| Autentikasi bawaan dari Laravel
-|-----
-*/
-Auth::routes();
->>>>>>> 2f0dc69406361a8d1f3308eb18f341117f5e92c2
