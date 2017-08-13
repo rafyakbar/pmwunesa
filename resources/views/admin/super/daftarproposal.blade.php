d3f9d3626ed19a21ef30f1ba06defa524559bdf3
bagasmuharom <bagashidayat45@gmail.com>
2017-08-13 16:30:06 +0700

    update halaman daftar proposal super admin

--- resources/views/admin/super/daftarproposal.blade.php
+++ resources/views/admin/super/daftarproposal.blade.php
@@ -5,14 +5,16 @@
     <ul>
         @foreach($proposal as $item)
             <li>
-                {{ $item->judul }}
+                {{-- {{ $item->judul }}
                 <form action="{{ route('editreviewer') }}" method="post">
                     {{ csrf_field() }}
                     {{ method_field('put') }}
                     <input type="hidden" name="judul" value="{{ $item->judul }}">
                     <input type="hidden" name="id_proposal" value="{{ $item->id }}">
                     <input type="submit" name="submit" value="set reviewer">
-                </form>
+                </form> --}}
+                {{ $item->judul }}
+                <a href="{{ route('edit.reviewer',['idproposal' => $item->id]) }}" class="btn btn-primary">Atur Reviewer</a>
             </li>
         @endforeach
     </ul>
