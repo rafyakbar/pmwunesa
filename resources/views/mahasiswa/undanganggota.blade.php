2a1b8fe98624c09ac8f0a6138156c15e42db8b38
bagasmuharom <bagashidayat45@gmail.com>
2017-08-17 16:03:23 +0700

    update blade undang anggota

--- resources/views/mahasiswa/undanganggota.blade.php
+++ resources/views/mahasiswa/undanganggota.blade.php
@@ -5,151 +5,160 @@
 @endpush
 
 @section('content')
-    <div class="row">
-        <div class="col-lg-4">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Undangan yang sedang dikirim</h4>
+
+    @if(!Auth::user()->mahasiswa()->punyaTim())
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Undangan yang sedang dikirim</h4>
+                    </div>
+                    <div class="card-content">
+                        @if(Auth::user()->mahasiswa()->undanganTimKetua()->count() > 0)
+                            @foreach (Auth::user()->mahasiswa()->undanganTimKetua()->get() as $undangan)
+                                <p>{{ $undangan->pengguna()->nama }} <a href="{{ route('hapus.undangan.tim',['id' => $undangan->id_pengguna]) }}" class="btn btn-danger btn-sm">Hapus</a></p>
+                            @endforeach
+                        @else
+                            <p class="alert alert-warning">Maaf, anda belum mengirim undangan ke siapapun</p>
+                        @endif
+                    </div>
                 </div>
-                <div class="card-content">
-                    @if(Auth::user()->mahasiswa()->undanganTimKetua()->count() > 0)
-                        @foreach (Auth::user()->mahasiswa()->undanganTimKetua() as $undangan)
-                            <p>{{ $undangan }}</p>
-                        @endforeach
-                    @else
-                        <p class="alert alert-warning">Maaf, anda belum mengirim undangan ke siapapun</p>
-                    @endif
+            </div>
+            <div class="col-lg-8">
+                <form class="card" action="{{ route('cari.mahasiswa') }}" method="get" id="cari-anggota">
+
+                    <input  type="text" name="nama" placeholder="Cari Mahasiswa" autofocus/>
+
+                    <button type="submit"><i class="fa fa-search"></i></button>
+
+                </form>
+
+                <div class="card">
+                    <div class="card-header" data-background-color="blue">
+                        <h4>Hasil Pencarian</h4>
+                    </div>
+                    <div class="card-content table-responsive">
+                        <div id="belum-cari" style="text-align:center;">
+                            <i class="fa fa-user fa-5x"></i>
+                            <p style="font-size:30px;padding-top:10px;">Mulailah untuk mencari anggota timmu</p>
+                        </div>
+                        <div id="not-found" style="text-align:center;display:none">
+                            <i class="fa fa-user-times fa-5x"></i>
+                            <p style="font-size:30px;padding-top:10px;">Tidak menemukan user yang anda cari</p>
+                        </div>
+                        <table class="table table-striped" style="display:none">
+                            <thead>
+                                <tr class="text-default">
+                                    <th>Nama</th>
+                                    <th>Asal</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody id="hasil-pencarian">
+                            </tbody>
+                        </table>
+                        <div id="hasil-pencarian"></div>
+                        <form action="{{ route('undang.anggota') }}" method="post" style="display: none" id="undang-anggota">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="untuk" />
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-8">
-            <form class="card" action="{{ route('cari.mahasiswa') }}" method="get" id="cari-anggota">
-
-                <input  type="text" name="nama" placeholder="Cari Mahasiswa" autofocus/>
-
-                <button type="submit"><i class="fa fa-search"></i></button>
-
-            </form>
-
-            <div class="card">
-                <div class="card-header" data-background-color="blue">
-                    <h4>Hasil Pencarian</h4>
-                </div>
-                <div class="card-content table-responsive">
-                    <div id="belum-cari" style="text-align:center;">
-                        <i class="fa fa-user fa-5x"></i>
-                        <p style="font-size:30px;padding-top:10px;">Mulailah untuk mencari anggota timmu</p>
+    @else
+        <div class="row">
+            <div class="col-lg-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Tim Anda</h4>
                     </div>
-                    <div id="not-found" style="text-align:center;display:none">
-                        <i class="fa fa-user-times fa-5x"></i>
-                        <p style="font-size:30px;padding-top:10px;">Tidak menemukan user yang anda cari</p>
+                    <div class="card-content">
+                        <ul>
+                            @foreach(Auth::user()->mahasiswa()->proposal()->mahasiswa()->cursor() as $anggota)
+                                <li>{{ $anggota->pengguna()->nama }} {!! $anggota->pengguna()->isKetua() ? '<b>(Ketua)</b>' : '' !!}</li>
+                            @endforeach
+                        </ul>
                     </div>
-                    <table class="table table-striped" style="display:none">
-                        <thead>
-                            <tr class="text-default">
-                                <th>Nama</th>
-                                <th>Asal</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody id="hasil-pencarian">
-                            <tr>
-                                <td>Namanya</td>
-                                <td>Fakultasnya</td>
-                                <td><button class="btn btn-primary btn-sm">Undang</button></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <div id="hasil-pencarian"></div>
-                    <form action="{{ route('undang.anggota') }}" method="post" style="display: none" id="undang-anggota">
-                        {{ csrf_field() }}
-                        <input type="hidden" name="untuk" />
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
 
+    @push('js')
+        <script src="{{ asset('js/jquery.form.js') }}"></script>
+        <script type="text/javascript">
+        $(function () {
 
-
-    <!-- <form action="{{ route('cari.mahasiswa') }}" method="get" id="cari-anggota">
-
-    <input type="text" name="nama"/>
-
-    <input type="submit" value="cari"/>
-
-</form> -->
-
-@push('js')
-    <script src="{{ asset('js/jquery.form.js') }}"></script>
-    <script type="text/javascript">
-    $(function () {
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
-            }
-        })
-
-        var inputCari = $('#cari-anggota')
-
-        var undangAnggota = $('#undang-anggota')
-
-        var hasilPencarian = $('#hasil-pencarian')
-
-        undangButton = function (id) {
-            var btn = $('<button></button>')
-            btn.attr('id', id)
-            btn.attr('class', 'undang-anggota btn btn-primary btn-sm')
-            btn.text('undang')
-            btn.click(function (e) {
-                e.preventDefault()
-                id = $(this).attr('id')
-                undangAnggota.find('input[name="untuk"]').val(id)
-                undangAnggota.submit();
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+                }
             })
 
-            return btn
-        }
+            var inputCari = $('#cari-anggota')
 
-        inputCari.ajaxForm({
-            success: function (response) {
-                $('#belum-cari').hide()
-                hasilPencarian.find('tr').remove()
-                if(response.length == 0){
-                    hasilPencarian.parent().hide()
-                    $('#not-found').show()
+            var undangAnggota = $('#undang-anggota')
+
+            var hasilPencarian = $('#hasil-pencarian')
+
+            undangButton = function (id) {
+                var btn = $('<button></button>')
+                btn.attr('id', id)
+                btn.attr('class', 'undang-anggota btn btn-primary btn-sm')
+                btn.text('undang')
+                btn.click(function (e) {
+                    e.preventDefault()
+                    id = $(this).attr('id')
+                    undangAnggota.find('input[name="untuk"]').val(id)
+                    undangAnggota.submit()
+                })
+
+                return btn
+            }
+
+            inputCari.ajaxForm({
+                success: function (response) {
+                    $('#belum-cari').hide()
+                    hasilPencarian.find('tr').remove()
+                    if(response.length == 0){
+                        hasilPencarian.parent().hide()
+                        $('#not-found').show()
+                    }
+                    else{
+                        $('#not-found').hide()
+                        hasilPencarian.parent().show()
+                        for (value in response) {
+                            var baris = $('<tr></tr>')
+                            var nama = $('<td></td>').text(response[value].nama)
+                            var asal = $('<td></td>').text('asal')
+                            var aksi = undangButton(response[value].id)
+                            baris.append(nama)
+                            baris.append(asal)
+                            baris.append(aksi)
+                            hasilPencarian.append(baris)
+                            // var fakultas = $('<td></td>').text(response[value].id)
+                        }
+                    }
                 }
-                else{
-                    $('#not-found').hide()
-                    hasilPencarian.parent().show()
-                for (value in response) {
-                    var baris = $('<tr></tr>')
-                    var nama = $('<td></td>').text(response[value].nama)
-                    var asal = $('<td></td>').text('asal')
-                    var aksi = undangButton(response[value].id)
-                    baris.append(nama)
-                    baris.append(asal)
-                    baris.append(aksi)
-                    hasilPencarian.append(baris)
-                    // var fakultas = $('<td></td>').text(response[value].id)
+            });
+
+            inputCari.find('input[type="text"]').on('keyup', function () {
+                if ($(this).val().length > 1)
+                $(this).parent().submit()
+            })
+
+            undangAnggota.ajaxForm({
+                success : function (response) {
+                    swal({
+                        title : response.error == 0 ? 'Berhasil' : 'Gagal',
+                        text : response.message,
+                        type : response.error == 0 ? 'success' : 'error'
+                    })
                 }
-            }
-            }
-        });
-
-        inputCari.find('input[type="text"]').on('keyup', function () {
-            if ($(this).val() !== '')
-            $(this).parent().submit()
+            })
         })
-
-        undangAnggota.ajaxForm({
-            success : function (response) {
-                alert(response.message)
-            }
-        })
-    })
-    </script>
-@endpush
-{{--@endif--}}
+        </script>
+    @endpush
+    {{--@endif--}}
 @endsection
