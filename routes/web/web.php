4bd8f06fdd8085f26f49a11e5b9d361ad2a0a463
bagasmuharom <bagashidayat45@gmail.com>
2017-08-24 21:36:37 +0700

    Update route untuk web.php

--- routes/web/web.php
+++ routes/web/web.php
@@ -5,13 +5,13 @@
  * maka user akan diarahkan ke halaman dashboard
  */
 
-Route::get('/',function(){
+Route::get('/', function () {
     return redirect()->route('dashboard');
 });
 
-Route::group(['middleware' => 'auth'], function(){
+Route::group(['middleware' => 'auth'], function () {
 
-    Route::group(['middleware' => 'profil'], function(){
+    Route::group(['middleware' => 'profil'], function () {
 
         /**
          * Jika user mmebuka halaman dashboard, maka akan
@@ -19,90 +19,90 @@ Route::group(['middleware' => 'auth'], function(){
          * Jika belum, maka akan diarahkan ke halaman login.
          */
         Route::get('dashboard', [
-            'uses'  => 'DashboardController@index',
-            'as'    => 'dashboard'
+            'uses' => 'DashboardController@index',
+            'as' => 'dashboard'
         ]);
 
-        Route::group(['prefix' => 'cari'], function(){
+        Route::group(['prefix' => 'cari'], function () {
 
-            Route::get('carimahasiswa',[
-                'uses'  => 'UserController@cariMahasiswa',
-                'as'    => 'cari.mahasiswa'
+            Route::get('carimahasiswa', [
+                'uses' => 'UserController@cariMahasiswa',
+                'as' => 'cari.mahasiswa'
             ]);
 
         });
 
-    });
+        Route::group(['prefix' => 'unduh'], function () {
 
-    Route::get('pengaturan', function(){
-         return view('pengaturan');
-    })->name('pengaturan');
+            Route::post('proposal', [
+                'uses' => 'ProposalController@unduh',
+                'as' => 'unduh.proposal',
+            ]);
 
-    Route::group(['prefix' => 'ubah'], function(){
+            Route::post('proposal/final', [
+                'uses' => 'ProposalFinalController@unduh',
+                'as' => 'unduh.proposal.final'
+            ]);
 
-        Route::get('password',function(){
-            return view('ubahpassword');
-        })->name('ubah.password');
+            Route::post('laporan/kemajuan', [
+                'uses' => 'LaporanController@unduh',
+                'as' => 'unduh.laporan.kemajuan',
+            ]);
 
-        Route::post('profil',[
-            'uses'  => 'UserController@editProfil',
-            'as'    => 'ubah.profil'
+            Route::post('laporan/akhir', [
+                'uses' => 'LaporanAkhirController@unduh',
+                'as' => 'unduh.laporan.akhir',
+            ]);
+
+        });
+
+        Route::get('data/proposal', [
+            'uses' => 'ProposalController@dataAjax',
+            'as' => 'data.proposal.ajax'
+        ]);
+
+        Route::group(['prefix' => 'lihat'], function () {
+
+            Route::get('proposal/{id}', [
+                'uses' => 'Page\PageController@proposalDetail',
+                'as' => 'lihat.proposal'
+            ]);
+
+            Route::get('review/{id}', [
+                'uses' => 'Page\PageController@lihatHasilReview',
+                'as' => 'lihat.hasil.review'
+            ]);
+
+        });
+
+        Route::post('request/pembimbing', [
+            'uses' => 'HakAksesController@requestDosenPembimbing',
+            'as' => 'request.pembimbing'
+        ]);
+
+        Route::post('request/reviewer', [
+            'uses' => 'HakAksesController@requestReviewer',
+            'as' => 'request.reviewer'
         ]);
 
     });
 
-    Route::post('request/pembimbing',[
-        'uses'  => 'HakAksesController@requestDosenPembimbing',
-        'as'    => 'request.pembimbing'
-    ]);
+    Route::get('pengaturan', function () {
+        return view('pengaturan');
+    })->name('pengaturan');
 
-    Route::post('request/reviewer',[
-        'uses'  => 'HakAksesController@requestReviewer',
-        'as'    => 'request.reviewer'
-    ]);
+    Route::group(['prefix' => 'ubah'], function () {
 
-});
+        Route::get('password', function () {
+            return view('ubahpassword');
+        })->name('ubah.password');
 
-Route::group(['prefix' => 'unduh'],function (){
+        Route::post('profil', [
+            'uses' => 'UserController@editProfil',
+            'as' => 'ubah.profil'
+        ]);
 
-    Route::post('proposal', [
-        'uses'  => 'ProposalController@unduh',
-        'as'    => 'unduh.proposal',
-    ]);
-
-    Route::post('proposal/final',[
-        'uses'  => 'ProposalFinalController@unduh',
-        'as'    => 'unduh.proposal.final'
-    ]);
-
-    Route::post('laporan/kemajuan',[
-        'uses'  => 'LaporanController@unduh',
-        'as'    => 'unduh.laporan.kemajuan',
-    ]);
-
-    Route::post('laporan/akhir',[
-        'uses'  => 'LaporanAkhirController@unduh',
-        'as'    => 'unduh.laporan.akhir',
-    ]);
-
-});
-
-Route::get('data/proposal',[
-    'uses'  => 'ProposalController@dataAjax',
-    'as'    => 'data.proposal.ajax'
-]);
-
-Route::group(['prefix' => 'lihat'], function (){
-
-    Route::get('proposal/{id}',[
-        'uses'  => 'Page\PageController@proposalDetail',
-        'as'    => 'lihat.proposal'
-    ]);
-
-    Route::get('review/{id}',[
-        'uses' => 'Page\PageController@lihatHasilReview',
-        'as' => 'lihat.hasil.review'
-    ]);
+    });
 
 });
 
