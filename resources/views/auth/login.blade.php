5e47f66c97c473e520156c6e30fe824845f52df7
BagasMuharom <bagashidayat45@gmail.com>
2017-10-04 16:58:04 +0700

    Menambah csrf field untuk reset password :))

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -100,6 +100,9 @@
                 <div class="panel-default login-panel panel">
                     <div class="panel-body">
                         <form role="form" action="{{ route('reset.password') }}" method="post">
+                            
+                            {{ csrf_field() }}
+                            
                             <fieldset>
                                 <div class="form-group">
                                     <label style="color: #111111">Email</label>
