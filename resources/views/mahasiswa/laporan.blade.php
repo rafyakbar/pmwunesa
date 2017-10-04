95a6df21312c6fb0d0fb681d39c45c9020d6c337
BagasMuharom <bagashidayat45@gmail.com>
2017-10-04 17:23:18 +0700

    Fixing halaman laporan

--- resources/views/mahasiswa/laporan.blade.php
+++ resources/views/mahasiswa/laporan.blade.php
@@ -7,6 +7,7 @@
 @section('content')
 
     @if(Auth::user()->mahasiswa()->punyaTim())
+        @if(Auth::user()->mahasiswa()->punyaProposal())
         @if(Auth::user()->mahasiswa()->proposal()->lolos())
             @include('mahasiswa.part.info_laporan')
         @else
@@ -15,6 +16,13 @@
                     <p class="alert alert-primary">Tim anda belum dinyatakan lolos</p>
                 </div>
             </div>
+        @endif
+        @else
+            <div class="card">
+            <div class="card-content">
+                <p class="alert alert-primary">Ketua tim anda belu mengunggah proposal</p>
+            </div>
+        </div>
         @endif
     @else
 
