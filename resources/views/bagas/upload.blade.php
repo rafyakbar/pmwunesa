a20d9d4d19aa10723696a30b305719728a7f67e9
BagasMuharom <bagashidayat45@gmail.com>
2017-11-09 15:39:38 +0700

    Menghapus folder bagas pada views

--- resources/views/bagas/upload.blade.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form action="{{ route('upload') }}" method="post" enctype="multipart/form-data">
-        {{ csrf_field() }}
-            <input type="file" name="file"/>
-            <input type="submit"/>
-        </form>
-    </body>
-</html>
