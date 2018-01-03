520a760a0a9ff1c9d924779a261eac9503414d50
BagasMuharom <bagashidayat45@gmail.com>
2018-01-03 10:48:07 +0700

    Menghapus file yang tidak diperlukan

--- resources/views/mahasiswa/unggahlaporan.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Public
- * Date: 8/4/2017
- * Time: 9:22 AM
- */
--- resources/views/mahasiswa/unggahlaporanakhir.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Public
- * Date: 8/5/2017
- * Time: 7:04 PM
- */
